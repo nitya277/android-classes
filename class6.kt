3efaa4ef4aece0c72d196ae2e79f90c4236e826c
nitya277 <rockyy27712@gmail.com>
2024-09-02 13:10:57 +0530

    Add files via upload

---
+++ class6.kt
@@ -0,0 +1,72 @@
+//Functions
+
+
+//fun sum(a:Int=10,b:Int=20) {
+//    val c=a+b
+//    println(c)
+//}
+//fun main() {
+//    sum(20,30);
+//    sum();
+//}
+
+
+
+//fun sum(a:Int, b:Int){
+//    val c=a+b;
+//    println(c)
+//}
+//fun main(){
+//    sum(20,50);
+//}
+
+//fun main(){
+//    val a=10;
+//    val b=20;
+//    val result=sum(a,b);
+//    println("result is $result")
+//}
+//fun sum (a:Int,b:Int): Int{
+//    val c=a+b;
+//    return c;
+//}
+
+//f=9/5xC + 32
+//c=5/9XF - 32
+//
+//fun C2F(c:Int){
+//    var f=(1.8)*c +32;
+//    println(f)
+//}
+//fun F2C(f:Int){
+//    var c=(5/9)*f -32;
+//    println(c);
+//}
+//fun main(){
+//    var celsius = 30;
+//    var f=216;
+//    C2F(celsius);
+//    F2C(f);
+//}
+
+
+//recursions
+//class Student{
+//    fun result(){
+//        println("Pendoing")
+//    }
+//}
+//fun main(){
+//    var s1=Student()
+//    s1.result()
+//}
+
+
+//input output
+
+fun main(){
+    println("Enter Your name:")
+    var name = readln().toInt();
+    var frName = readln();
+    println("Your name is $name & ur friend name is $frName");
+}
